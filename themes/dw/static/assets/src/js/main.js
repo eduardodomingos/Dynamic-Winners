0d8090305419baaef15708cc0548db4652e8e566
Eduardo Domingos <edomingos@impresa.pt>
2016-07-11 12:34:59 +0100

    Add static folder

---
+++ themes/dw/static/assets/src/js/main.js
@@ -0,0 +1,98 @@
+(function($) {
+	$(document).ready(function(){
+
+		// Cache references to DOM elements for performance
+		//=======================================
+		var dom = {
+			$window:            				$(window),
+			$body:              				$('body'),
+			$HomeHeadlinesCarousel: 				$('.home #headlines .carousel'),
+			$HomeNewsCarousel: 						$('.home #news .carousel'),
+			$HomeServicesCarousel: 					$('.home #services .carousel'),
+			$HomeAthletesCarousel: 					$('.home #athletes .carousel')
+		};
+
+		// Home / Headlines carousel
+		//=======================================
+		dom.$HomeHeadlinesCarousel.slick({
+			mobileFirst: true,
+			autoplay: false,
+			dots: true,
+			arrows: false,
+			//appendDots: dom.$headlinesCarouselDotsContainer,
+			responsive: [
+				{
+					breakpoint: 1024,
+					settings: {
+						arrows: true
+					}
+				}
+			]
+		});
+
+		// Home / News carousel
+		//=======================================
+		dom.$HomeNewsCarousel.slick({
+			mobileFirst: true,
+			appendArrows: $('.home #news .arrows'),
+			responsive: [
+				{
+					breakpoint: 480,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2
+					}
+				}
+			]
+		});
+
+		// Home / Services carousel
+		//=======================================
+		dom.$HomeServicesCarousel.slick({
+			mobileFirst: true,
+			dots: true,
+			responsive: [
+				{
+					breakpoint: 480,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2
+					}
+				},
+				{
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 3
+					}
+				}
+			]
+		});
+
+		// Home / Athletes carousel
+		//=======================================
+		dom.$HomeAthletesCarousel.slick({
+			mobileFirst: true,
+			appendArrows: $('.home #athletes .arrows'),
+			dots: true,
+			responsive: [
+				{
+					breakpoint: 480,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2
+					}
+				},
+				{
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 3
+					}
+				}
+			]
+		});
+
+
+	});
+}(jQuery));
---
+++ themes/dw/static/grunt/concat.js
@@ -0,0 +1,10 @@
+module.exports = {
+	dist: {
+		src: [
+			'bower_components/slick-carousel/slick/slick.js',
+			'bower_components/jQuery.dotdotdot/src/jquery.dotdotdot.js',
+			'assets/src/js/main.js'
+		],
+		dest: 'assets/build/js/main.js'
+	}
+};
