49bc6319d0e7910911f99b1ebb0cc3e859189e0a
Eduardo Domingos <epcdomingos@gmail.com>
2016-07-12 21:45:28 +0100

    Work in progress...

--- themes/dw/static/assets/build/js/main.js
+++ themes/dw/static/assets/build/js/main.js
@@ -3023,15 +3023,14 @@
 		dom.$athleteDetailsCarousel.slick({
 			mobileFirst: true,
 			responsive: [
-				// {
-				// 	breakpoint: 1024,
-				// 	settings: {
-				// 		slidesToShow: 2,
-				// 		slidesToScroll: 2
-				// 	}
-				// }
+				{
+					breakpoint: 480,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 1
+					}
+				}
 			]
 		});
-
 	});
 }(jQuery));
--- themes/dw/static/assets/src/js/main.js
+++ themes/dw/static/assets/src/js/main.js
@@ -130,15 +130,14 @@
 		dom.$athleteDetailsCarousel.slick({
 			mobileFirst: true,
 			responsive: [
-				// {
-				// 	breakpoint: 1024,
-				// 	settings: {
-				// 		slidesToShow: 2,
-				// 		slidesToScroll: 2
-				// 	}
-				// }
+				{
+					breakpoint: 480,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 1
+					}
+				}
 			]
 		});
-
 	});
 }(jQuery));
