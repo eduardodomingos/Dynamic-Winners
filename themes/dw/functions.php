1d9f2d731ebd64c82ff21f5e59bcf439d89f030e
Pedro Nunes <preisnunes@gmail.com>
2016-07-16 11:41:50 +0100

    working in progress

--- themes/dw/functions.php
+++ themes/dw/functions.php
@@ -135,6 +135,11 @@ require get_template_directory() . '/inc/extras.php';
 require get_template_directory() . '/inc/customizer.php';
 
 /**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/theme-setup.php';
+
+/**
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
