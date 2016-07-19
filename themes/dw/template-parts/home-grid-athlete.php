3bbb6bd058c5c2b37f8cf1e00d9ec6cf4f580ab2
Pedro Nunes <preisnunes@gmail.com>
2016-07-19 01:45:37 +0100

    add widget athlete and post parts

---
+++ themes/dw/template-parts/home-grid-athlete.php
@@ -0,0 +1,12 @@
+<div class="slider__item">
+	<article class="card entry entry--athlete text-xs-center  m-b-0">
+		<div class="img-wrapper">
+			<p class="category">Defesa</p>
+			<a href=""><img alt="" srcset="http://placehold.it/224x168?text=4:3 224w, http://placehold.it/263x197?text=4:3 263w" sizes="(min-width: 480px) 50vw, 100vw" class="img-fluid"></a>
+		</div><!-- img-wrapper -->
+		<div class="card-block">
+			<h2 class="entry__title"><a href=""><?php the_title(); ?></a></h2>
+			<p class="entry__text card-text"><?php the_excerpt(); ?></p>
+		</div><!-- card-block -->
+	</article><!-- card -->
+</div><!-- slider__item -->
---
+++ themes/dw/template-parts/home-grid-post.php
@@ -0,0 +1,11 @@
+<div class="slider__item">
+	<article class="card entry m-b-0">
+		<a href=""><img alt="" srcset="http://placehold.it/224x168?text=4:3 224w, http://placehold.it/263x197?text=4:3 263w" sizes="(min-width: 480px) 50vw, 100vw" class="img-fluid"></a>
+		<div class="card-block p-l-0 p-r-0 p-b-0">
+			<p class="entry__date"><?php the_date('j F Y'); ?></p>
+			<h2 class="entry__title"><a href=""><?php the_title(); ?></a></h2>
+			<p class="entry__text card-text"><?php the_excerpt(); ?></p>
+			<a href="" class="entry__read-more">Ver mais</a>
+		</div><!-- card-block -->
+	</article><!-- card -->
+</div><!-- slider__item -->
