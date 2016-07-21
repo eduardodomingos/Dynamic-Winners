17b4d41710e3b99d14cb2da31842f3fda3582d14
Eduardo Domingos <epcdomingos@gmail.com>
2016-07-21 23:25:17 +0100

    Remove whitespace

--- themes/dw/template-parts/home-grid-contacts.php
+++ themes/dw/template-parts/home-grid-contacts.php
@@ -5,22 +5,22 @@ $phone = get_field('phone', $home_id );
 $email = get_field('email', $home_id );
 ?>
 <section id="contacts" class="band section">
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-12 col-lg-8 col-lg-offset-2">
-					<div class="section__header">
-						<h2 class="heading">Contacte-nos</h2>
-					</div><!-- section__header -->
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 col-lg-8 col-lg-offset-2">
+				<div class="section__header">
+					<h2 class="heading">Contacte-nos</h2>
+				</div><!-- section__header -->
 
-					<?php echo do_shortcode('[contact-form-7 id="393" title="Contact form 1" html_class="contact-form"]');?>
+				<?php echo do_shortcode('[contact-form-7 id="393" title="Contact form 1" html_class="contact-form"]');?>
 
-					<?php wp_nav_menu( array( 'theme_location' => 'social_footer', 'container'=> false, 'menu_id' => 'social-menu', 'menu_class' => 'social text-xs-center', 'link_before' => '<span class="sr-only">', 'link_after' => '</span>' ) ); ?>
+				<?php wp_nav_menu( array( 'theme_location' => 'social_footer', 'container'=> false, 'menu_id' => 'social-menu', 'menu_class' => 'social text-xs-center', 'link_before' => '<span class="sr-only">', 'link_after' => '</span>' ) ); ?>
 
-					<div class="text-xs-center">
-						<p><a href="tel:+<?php echo str_replace(' ', '', $phone); ?>" class="contacts__link">+<?php echo $phone; ?></a></p>
-						<p><a href="mailto:geral@dynamicwinners.com" class="contacts__link"><?php echo $email; ?></a></p>
-					</div>
-				</div><!-- col -->
-			</div><!-- row -->
-		</div><!-- container -->
-	</section><!-- contacts -->
+				<div class="text-xs-center">
+					<p><a href="tel:+<?php echo str_replace(' ', '', $phone); ?>" class="contacts__link">+<?php echo $phone; ?></a></p>
+					<p><a href="mailto:geral@dynamicwinners.com" class="contacts__link"><?php echo $email; ?></a></p>
+				</div>
+			</div><!-- col -->
+		</div><!-- row -->
+	</div><!-- container -->
+</section><!-- contacts -->
