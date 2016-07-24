daf1e80acb37796061a78c5246756d2268f88143
Pedro Nunes <preisnunes@gmail.com>
2016-07-24 17:52:41 +0100

    add athlete features template

---
+++ themes/dw/template-parts/athlete-features.php
@@ -0,0 +1,19 @@
+<div class="slider__item">
+	<div class="row band">
+
+		<div class="col-md-5 col-lg-4">
+			<div class="media hidden-sm-down">
+				<a class="media-left" href="#">
+					<img class="media-object" src="<?php echo get_bloginfo('template_directory') ?>/assets/build/img/<?php echo $icon_name; ?>.svg" alt="academic icon">
+				</a><!-- media-left -->
+				<div class="media-body">
+					<h4 class="media-heading"><?php echo $name; ?></h4>
+				</div><!-- media-body -->
+			</div><!-- media -->
+		</div><!-- col -->
+		<div class="col-md-7 col-lg-8">
+			<?php echo $content;  ?>
+		</div><!-- col -->
+
+	</div><!-- row -->
+</div><!-- slider__item -->
