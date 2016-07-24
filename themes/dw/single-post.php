68a98575eadf3093da56c9b48df697143df90d29
Pedro Nunes <preisnunes@gmail.com>
2016-07-24 20:43:55 +0100

    display categories at single post

--- themes/dw/single-post.php
+++ themes/dw/single-post.php
@@ -38,7 +38,15 @@ $nav_classes = array('prev', 'next');
 						</div><!-- col -->
 						<div class="col-md-6">
 							<header class="post__header">
-								<p class="post__category">Regionais 2016</p>
+								<ul>
+								<?php 
+								$categories = wp_get_object_terms(get_the_ID(), 'category', array('fields' => 'names'));
+								if( !empty( $categories ) ) { ?>
+									<?php foreach( $categories as $category ) { ?>
+									<li class="post__category"><?php echo $category; ?> 
+								<?php } } ?>
+								</ul>
+								
 								<h3 class="post__title"><?php echo get_the_title(); ?></h3>
 								<?php echo dw_posted_on(); ?>
 
