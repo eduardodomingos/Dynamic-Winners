d167c1d22f9a0b8f88d8662a76b12e98126e4824
Eduardo Domingos <edomingos@impresa.pt>
2016-07-27 13:20:15 +0100

    Minor markup changes

--- themes/dw/template-parts/home-grid-athlete.php
+++ themes/dw/template-parts/home-grid-athlete.php
@@ -11,14 +11,12 @@ $position = __(dw_get_athlete_positions( $post->ID ), 'dw');
 <div class="slider__item">
 <?php endif; ?>
 	<article class="card entry entry--athlete text-xs-center m-b-0 <?php echo $is_last ? 'last' : ''; ?>">
-		<div class="img-wrapper">
-			<?php if( !empty( $position ) ): ?>
-				<p class="category"><?php echo implode('/', $position); ?></p>
-			<?php endif; ?>
-			<a href="<?php echo get_the_permalink();?>"><img alt="" srcset="<?php echo $thumbnail_small[0]; ?> 224w, <?php echo $thumbnail_medium[0]; ?> 263w" sizes="(min-width: 480px) 50vw, 100vw" class="img-fluid"></a>
-		</div><!-- img-wrapper -->
-		<div class="card-block">
+		<a href="<?php echo get_the_permalink();?>"><img alt="" srcset="<?php echo $thumbnail_small[0]; ?> 224w, <?php echo $thumbnail_medium[0]; ?> 263w" sizes="(min-width: 480px) 50vw, 100vw" class="img-fluid"></a>
+		<div class="card-block p-l-0 p-r-0">
 			<h2 class="entry__title"><a href="<?php echo get_the_permalink();?>"><?php echo get_the_title(); ?></a></h2>
+			<?php if( !empty( $position ) ): ?>
+				<p class="category"><?php echo implode(' / ', $position); ?></p>
+			<?php endif; ?>
 		</div><!-- card-block -->
 	</article><!-- card -->
 <?php if($is_slider):?>
