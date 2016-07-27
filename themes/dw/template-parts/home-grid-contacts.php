54f29f43d6d34414b36c83f893688e44c5832599
Eduardo Domingos <edomingos@impresa.pt>
2016-07-27 15:49:20 +0100

    Add another phone, address and gps

--- themes/dw/template-parts/home-grid-contacts.php
+++ themes/dw/template-parts/home-grid-contacts.php
@@ -2,7 +2,10 @@
 
 $home_id = dynamic_get_active_homepage();
 $phone = get_field('phone', $home_id );
+$phone_alt = get_field('phone_alt', $home_id );
 $email = get_field('email', $home_id );
+$address = get_field('address', $home_id );
+$gps = get_field('gps', $home_id );
 ?>
 <section id="contacts" class="band section">
 	<div class="container">
@@ -23,8 +26,13 @@ $email = get_field('email', $home_id );
 				<?php wp_nav_menu( array( 'theme_location' => 'social_footer', 'container'=> false, 'menu_id' => 'social-menu', 'menu_class' => 'social text-xs-center', 'link_before' => '<span class="sr-only">', 'link_after' => '</span>' ) ); ?>
 
 				<div class="text-xs-center">
-					<p><a href="tel:+<?php echo str_replace(' ', '', $phone); ?>" class="contacts__link">+<?php echo $phone; ?></a></p>
-					<p><a href="mailto:geral@dynamicwinners.com" class="contacts__link"><?php echo $email; ?></a></p>
+					<p>
+						<a href="tel:+<?php echo str_replace(' ', '', $phone); ?>" class="contacts__link">+<?php echo $phone; ?></a><br>
+						<a href="tel:+<?php echo str_replace(' ', '', $phone_alt); ?>" class="contacts__link">+<?php echo $phone_alt; ?></a>
+					</p>
+					<p><?php echo $address; ?></p>
+					<p><?php echo $gps; ?></p>
+					<p><a href="mailto:<?php echo $email; ?>" class="contacts__link"><?php echo $email; ?></a></p>
 				</div>
 			</div><!-- col -->
 		</div><!-- row -->
