717e19e111eec928cd94fd625397a88c1fb0f85c
Eduardo Domingos <edomingos@impresa.pt>
2016-07-28 14:46:33 +0100

    Fix order

--- themes/dw/front-page.php
+++ themes/dw/front-page.php
@@ -17,12 +17,12 @@ get_header(); ?>
 <?php dynamic_get_headline_manchetes(); ?>
 
 <div class="band band--primary">
-	
+
 	<?php dynamic_sidebar( 'dynamic-homepage-sections' ); ?>
 
-</div>
+	<?php get_template_part( 'template-parts/home-grid', 'sponsors' ); ?>
 
-<?php get_template_part( 'template-parts/home-grid', 'sponsors' ); ?>
+</div>
 
 <?php get_template_part( 'template-parts/home-grid', 'contacts' ); ?>
 
